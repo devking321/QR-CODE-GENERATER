4ba8d4a274c760d3c6665b38072fe6f00611982f
its me <iyourdream619@gmail.com>
2025-12-29 21:35:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,46 +4,52 @@ const qrDiv = document.querySelector(".qr");
 const generate = document.getElementById("generate");
 const download = document.getElementById("download");
 
-// hide download button at start
-download.style.visibility = "hidden";
+// download.disabled=true;
 
 generate.addEventListener("click", () => {
-    if (input.value.trim().length > 0) {
+    if (input.value.length >= 1) {
         genQrCode();
-        download.style.visibility = "visible";
-    } else {
-        alert("Enter Text or URL");
+        download.style.visibility="visible"
     }
+    else {
+        alert("Enter Text or URL")
+    }
+
+    
 });
 
 function genQrCode() {
-    qrDiv.innerHTML = ""; // clear old QR
-    download.disabled = false;
-
+    download.disabled=false;
+    qrDiv.innerHTML = ""
     new QRCode(qrDiv, {
         text: input.value,
-        width: Number(size.value),
-        height: Number(size.value),
-        colorDark: "#000000",
-        colorLight: "#ffffff"
-    });
-}
+        height: size.value,
+        width: size.value,
+        colorLight: "white",
+        colorDark: "black"
+    })
+};
 
-download.addEventListener("click", (e) => {
+download.addEventListener("click",(e)=>{
     e.preventDefault();
+   const img = document.querySelector(".qr img");
 
-    const img = qrDiv.querySelector("img");
-
-    if (!img) {
-        alert("Generate QR Code first");
-        return;
-    }
-
+if(img !== null){
+     fetch(img.src)
+   .then( res=> res.blob())
+   .then( (blob)=>{
+    const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
-    a.href = img.src;
-    a.download = "QR_Code.png";
+    a.href=url;
+    a.download="QR_Code.png";
 
     document.body.appendChild(a);
     a.click();
-    document.body.removeChild(a);
-});
+
+   document.body.removeChild(a);
+    URL.revokeObjectURL(url); 
+   } )  
+
+}
+   
+})
